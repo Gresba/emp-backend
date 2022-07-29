87b131e00a85f3500d09fbcdc1eb242e190ab5ed
gresba <kimpaul0709@gmail.com>
2022-07-29 14:45:18 -0400

    Added Cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ import db from "./db/connection.js";
 import routes from "./routes/index.js";
 
 import express from "express";
+import cors form "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", routes);
 
