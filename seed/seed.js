1d7e943b8f0fcc6a272ec9030ce3a29fbde50f5c
gresba <kimpaul0709@gmail.com>
2022-07-29 14:33:09 -0400

    bug fix

--- seed/seed.js
+++ seed/seed.js
@@ -1,31 +1,31 @@
 import db from "../db/connection.js";
-import Picture from "../models/Picture.js";
+import Picture from "../models/picture.js";
 import fetch from "node-fetch";
 
 const apiUrl = "https://api.artic.edu/api/v1/artworks"
 
 const seedDb = async () => {
-    await db.dropDatabase();
+  await db.dropDatabase();
 
-    const response = await fetch(apiUrl);
-    const pictures = await response.json();
+  const response = await fetch(apiUrl);
+  const pictures = await response.json();
 
-    const pictureData = [];
+  const pictureData = [];
 
-    pictures.data.forEach(picture => {
-        pictureData.push({
-            Artist: picture.artist_title,
-            Title: picture.title,
-            ImageId: picture.image_id,
-            Year: picture.date_start,
-            Tags: picture.term_titles,
-            Comments: [] 
-        })
+  pictures.data.forEach(picture => {
+    pictureData.push({
+      Artist: picture.artist_title,
+      Title: picture.title,
+      ImageId: picture.image_id,
+      Year: picture.date_start,
+      Tags: picture.term_titles,
+      Comments: []
     })
+  })
 
-    await Picture.insertMany(pictureData);
+  await Picture.insertMany(pictureData);
 
-    db.close();
+  db.close();
 }
 
 seedDb();
