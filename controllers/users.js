dea01d4c162417e4aaaf1cf6a6e0a5c39d2b877d
gresba <kimpaul0709@gmail.com>
2022-08-01 13:03:12 -0400

    Added Authentication

--- controllers/users.js
+++ controllers/users.js
@@ -6,9 +6,9 @@ import bcrypt from "bcryptjs";
 let secret = process.env.JWT_SECRET || "123";
 
 let generateToken = async (id) => {
-    return await Jwt.sign({ id: id}, secret ({
+    return await Jwt.sign({ id: id}, secret, {
         expiresIn: "7d",
-    }));
+    });
 };
 
 export const getUsers = async (req, res) => {
@@ -69,7 +69,7 @@ export const createUser = async (req, res) => {
     }
 
     // Check if the user exist
-    const doesExist = await User.findOne({ email: Email });
+    const doesExist = await User.findOne({ Email: Email });
     if(doesExist) {
         return res.status(400).json({ error: "User already exist"});
     }
@@ -85,11 +85,59 @@ export const createUser = async (req, res) => {
 
     await user.save();
 
-    res.status(201).json(user);
+    res.status(201).json({
+        id: user._id,
+        name: user.Full_Name,
+        email: user.Email,
+        token: await generateToken(user._id),
+    });
   } catch (err) {
     console.log(err)
     res.status(500).json({ error: err.message });
   }
+}
+
+export const login = async (req, res) =>
+{
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ Email: email });
+
+        if( user && ( await bcrypt.compare(password, user.Password))){
+            res.json({
+                _id: user._id,
+                name: user.Full_Name,
+                email: user.Email,
+                token: await generateToken(user._id),
+            });
+        }else{
+            res.status(403).json({
+                error: "Invalid Credentials",
+            });
+        }
+    } catch (err){
+        res.status(500).json({error: err.message});
+    }
+}
+
+export const profile = async (req, res) => {
+    let requestHeaders = req.headers.authorization;
+
+    if (requestHeaders && requestHeaders.startsWith("Bearer")){
+        try {
+            let token = requestHeaders.split(" ")[1];
+            
+            const decoded = Jwt.verify(token, secret);
+
+            let userProfile = await User.findById(decoded.id).select("-Password");
+            
+            res.json(userProfile);
+        } catch {
+            res.status(401).json({ error: "Not Authorized / Incorrect Token"});
+        }
+    } else {
+        res.status(400).json({ error: "Missing Token"});
+    }
 }
 
 export const updateUser = async (req, res) => {
